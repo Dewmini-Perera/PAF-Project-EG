3f24fcfbe9b7cab5471816b07a415dded72b9b69
Savindi Fernando <89602336+SavindiFernando@users.noreply.github.com>
2022-04-26 22:57:45 +0530

    Savi Edit

--- src/main/java/com/egsystem/database/PriceTableManagement.java
+++ src/main/java/com/egsystem/database/PriceTableManagement.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class PriceTableManagement {
-
+	//connect database
 	public Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");  
